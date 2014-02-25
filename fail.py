a50930501d8ae1229b49bd25ab14b420a5aa7dc6
Aarne Aramaa <Arnyland@gmail.com>
2014-02-25 14:14:02 +0200

    Redone with AST module

--- fail.py
+++ fail.py
@@ -12,6 +12,9 @@ ArgumentFive = "Hello"
 ArgumentSix = [1, 2, 3]
 ArgumentSeven = (1, 2, 3)
 ArgumentEight = ArgumentOne, ArgumentTwo
+ArgumentNine = {"yes", "no"}
+ArgumentTen = True
+ArgumentEleven = [1,2,[3,4,[5,6]]]
 print(ArgumentEight)
 if(ArgumentOne==100):
     print("Printing this")
